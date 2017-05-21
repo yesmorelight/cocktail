5a64fdc22d8c3a2c4620bc406a53169c294b984a
Steven Best <sr.best@live.com>
2017-05-21 11:13:22 +0100

    upload of code

---
+++ script.js
@@ -0,0 +1,210 @@
+//BOURBON BOURBON
+//BOURBON BOURBON
+$(document).ready(function(){
+  $('.bourbon_list').hide();
+  $('.bourbon').click(function() {
+    $('.bourbon_list').slideToggle(400);
+  });
+});
+
+
+//HIDES OLD FASHIONED
+$(document).ready(function(){
+  $('.of_method').hide();
+  $('.of').click(function() {
+    $('.of_method').slideToggle(400);
+  });
+});
+
+//HIDES WHISKEY SOUR
+$(document).ready(function() {
+  $('.ws_method').hide();
+  $('.ws').click(function(){
+    $('.ws_method').slideToggle(400);
+  });
+});
+
+//HIDES MANHATTAN
+$(document).ready(function() {
+  $('.m_method').hide();
+  $('.m').click(function(){
+    $('.m_method').slideToggle(400);
+  });
+});
+
+//MINT JULEP
+$(document).ready(function(){
+  $('.mj_method').hide();
+  $('.mj').click(function(){
+    $('.mj_method').slideToggle(400);
+  });
+});
+
+
+//GIN GIN GIN GIN
+//GIN GIN GIN GIN
+$(document).ready(function(){
+  $('.gin_list').hide();
+  $('.gin').click(function(){
+    $('.gin_list').slideToggle(400);
+  });
+});
+
+//HIDES SOUTHSIDE
+$(document).ready(function(){
+  $('.ss_method').hide();
+  $('.ss').click(function() {
+    $('.ss_method').slideToggle(400);
+  });
+});
+
+//HIDES G&T 
+$(document).ready(function(){
+  $('.gt_method').hide();
+  $('.gt').click(function() {
+    $('.gt_method').slideToggle(400);
+  });
+});
+
+//HIDES SOUTHERN BRIDE
+$(document).ready(function(){
+  $('.sb_method').hide();
+  $('.sb').click(function() {
+    $('.sb_method').slideToggle(400);
+  });
+});
+
+
+// RUM RUM RUM
+//RUM RUM RUM
+$(document).ready(function(){
+  $('.rum_list').hide();
+  $('.rum').click(function(){
+    $('.rum_list').slideToggle(400);
+  });
+});
+
+// HIDES MAI-TAI
+$(document).ready(function(){
+  $('.mt_method').hide();
+  $('.mt').click(function() {
+    $('.mt_method').slideToggle(400);
+  });
+});
+
+//HIDES BAHAMA MAMA
+$(document).ready(function(){
+  $('.bm_method').hide();
+  $('.bm').click(function(){
+    $('.bm_method').slideToggle(400);
+  });
+});
+
+//HIDES STRAWBERRY DIAQUIRI
+$(document).ready(function(){
+  $('.sd_method').hide();
+  $('.sd').click(function(){
+    $('.sd_method').slideToggle(400);
+  });
+});
+
+//TEQUILA TEQUILA
+//TEQUILA TEQUILA
+$(document).ready(function(){
+  $('.tequila_list').hide();
+  $('.tequila').click(function(){
+    $('.tequila_list').slideToggle(400);
+  });
+});
+
+// TEQUILA SUNRISE
+$(document).ready(function(){
+  $('.ts_method').hide();
+  $('.ts').click(function(){
+    $('.ts_method').slideToggle(400);
+  });
+});
+
+// MARGARITA
+$(document).ready(function(){
+  $('.ma_method').hide();
+  $('.ma').click(function(){
+    $('.ma_method').slideToggle(400);
+  });
+});
+
+//TEQUILA SLAMMER
+$(document).ready(function(){
+  $('.tsl_method').hide();
+  $('.tsl').click(function(){
+    $('.tsl_method').slideToggle(400);
+  });
+});
+
+// VODKA VODKA
+//VODKA VODKA
+$(document).ready(function(){
+  $('.vodka_list').hide();
+  $('.vodka').click(function(){
+    $('.vodka_list').slideToggle(400);
+  });
+});
+
+//SCREWDRIVER
+$(document).ready(function(){
+  $('.s_method').hide();
+  $('.s').click(function(){
+    $('.s_method').slideToggle(400);
+  });
+});
+
+//MOSCOW MULE
+$(document).ready(function(){
+  $('.mm_method').hide();
+  $('.mm').click(function(){
+    $('.mm_method').slideToggle(400);
+  });
+});
+
+//MELON BALL
+$(document).ready(function(){
+  $('.mb_method').hide();
+  $('.mb').click(function(){
+    $('.mb_method').slideToggle(400);
+  });
+});
+
+//CHAMPAGNE
+//CHAMPAGNE
+$(document).ready(function(){
+  $('.champagne_list').hide();
+  $('.champagne').click(function(){
+    $('.champagne_list').slideToggle(400);
+  });
+});
+
+// BELLINI
+$(document).ready(function(){
+  $('.bl_method').hide();
+  $('.bl').click(function(){
+    $('.bl_method').slideToggle(400);
+  });
+});
+
+
+//OTHER OTHER
+//OTHER OTHER
+$(document).ready(function(){
+  $('.more_list').hide();
+  $('.more').click(function(){
+    $('.more_list').slideToggle(400);
+  });
+});
+
+//MINNESOTA TIDAL WAVE
+$(document).ready(function(){
+  $('.mtw_method').hide();
+  $('.mtw').click(function(){
+    $('.mtw_method').slideToggle(400);
+  });
+});
